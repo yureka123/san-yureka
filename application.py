e4be78e3ab0e2cbceb1abd28121459e329adcc06
Sandesh <32417740+yureka123@users.noreply.github.com>
2024-09-12 11:53:33 +0530

    Create application.py

---
+++ application.py
@@ -0,0 +1,8 @@
+!pip install -q -U google-generativeai
+
+!npm install -g localtunnel@2.0.2
+
+!pip install streamlit langchain --quiet
+
+import google.generativeai as genai
+from google.colab import userdata
